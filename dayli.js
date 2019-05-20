ab29a73dfd309bb72de8ae5cd7a89590d09226d9
Avery Herkel <aherkel09@gmail.com>
2019-05-20 15:57:39 -0400

    Dayli class

--- dayli.js
+++ dayli.js
@@ -1,74 +1,75 @@
-function setDateText() {
-    var date = new Date();
-    $('#date').text(date.toDateString());
-}
-
-function createDate(docRef) {
-    docRef.set({
-        meditation: false,
-        composition: false,
-    }).catch(function(error) {
-        console.log(error);
-    });
-}
-
-function checkDate() {
-    var ref = getRef();
-    ref.get().then(function(doc) {
-        if (doc.exists) {
-            console.log(doc.data());
-            updateGoalsFromDB(doc.data());
-        } else {
-            createDate(ref);
+class Dayli {
+    constructor(year, date) {
+        this.docRef = firebase.firestore().collection(year).doc(date);
+    }
+    
+    getDoc() {
+        this.docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log(doc.data());
+                this.update(doc.data());
+            } else {
+                this.createDoc();
+            }
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+    
+    createDoc() {
+        this.docRef.set({
+            meditation: false,
+            composition: false,
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+    
+    update(data) {
+        var complete = true;
+        for (var d in data) {
+            if (data[d] == true) {
+                $('#' + d).fadeOut('slow');
+            } else if (data[d] == false) {
+                complete = false;
+            }
         }
-    }).catch(function(error) {
-        console.log(error);
-    });
-}
-
-function getRef() {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = String(date.getFullYear());
-    var monthAndDay = month + '-' + day;
-    return firebase.firestore().collection(year).doc(monthAndDay);
-}
-
-function updateGoalsFromDB(data) {
-    var complete = true;
-    for (var d in data) {
-        if (data[d] == true) {
-            $('#' + d).fadeOut('slow');
-        } else if (data[d] == false) {
-            complete = false;
+        if (complete) {
+            $('#content').fadeOut('slow', function() {
+                $('#complete').fadeIn('slow');
+            });
         }
     }
     
-    if (complete) {
-        $('#content').fadeOut('slow', function() {
-            $('#complete').fadeIn('slow');
+    markDone(goal) {
+        var data = {};
+        data[goal] = true;
+        this.docRef.update(data).then(function() {
+            this.getDoc();
+        }).catch(function(error) {
+            console.log(error);
         });
     }
 }
 
-function markDone(ev) {
-    var data = {};
-    var goal = $(ev.target).closest('.card').attr('id');
-    data[goal] = true;
-    var ref = getRef();
-    ref.update(data).then(function() {
-        checkDate();
-    }).catch(function(error) {
-        console.log(error);
-    });
+function getDate() {
+    var date = new Date();
+    $('#date').text(date.toDateString()).fadeIn('slow');
+    var year = String(date.getFullYear());
+    var date = String(date.getMonth()+1) + '-' + String(date.getDate());
+    return {
+        year: year,
+        date: date,
+    };
 }
 
 $(document).ready(function() {
-    setDateText();
-    checkDate();
+    var date = getDate();
+    var dayli = new Dayli(date.year, date.date);
+    dayli.getDoc();
     $('.card').click(function(ev) {
-        markDone(ev);
+        var goal = $(ev.target).closest('.card').attr('id');
+        dayli.markDone(goal);
     });
     $('#content').fadeIn('slow');
 });
