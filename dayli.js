e7662519dc81f82746c8947930bebc7bf2743fb1
Avery Herkel <aherkel09@gmail.com>
2019-05-20 13:55:55 -0400

    get and set firestore data

--- dayli.js
+++ dayli.js
@@ -3,14 +3,57 @@ function setDate() {
     $('#date').text(date.toDateString());
 }
 
-function markDone(ev) {
-    $(ev.target).closest('.card').fadeOut('slow');
+function createDate(year, date) {
+    firebase.firestore().collection(year).doc(date).set({
+        complete: false,
+    }).then(function(doc) {
+        updateGoals(doc, create=true);
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+function checkDate() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var monthAndDay = month + '-' + day;
+    
+    firebase.firestore().collection(year).doc(monthAndDay).get().then(function(doc) {
+        if (doc.exists) {
+            updateGoals(doc);
+        } else {
+            createDate(year, monthAndDay);
+        }
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+function updateGoals(doc, create=false) {
+    var complete = true;
+    var goals = ['meditation', 'composition'];
+    
+    for (var g in goals) {
+        if (create) {
+            doc.set({g: false});
+        } else if (doc.data()[g] == true) {
+            $('#' + g).fadeOut('slow');
+        } else {
+            complete = false;
+        }
+    }
+    
+    if (complete) {
+        console.log('Done for the day!');
+    }
 }
 
 $(document).ready(function() {
     setDate();
     $('.card').click(function(ev) {
-        markDone(ev);
+        checkDate();
     });
     $('#content').fadeIn('slow');
 });
