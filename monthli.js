946a9113b53d98c4824838d569f47f12006375af
Avery Herkel <aherkel09@gmail.com>
2019-05-22 08:30:33 -0400

    get completed docs

--- monthli.js
+++ monthli.js
@@ -1,1 +1,19 @@
+class Monthli {
+    constructor(year) {
+        this.year = year;
+        this.docRef = firebase.firestore().collection(this.year);
+    }
+    
+    getComplete() {
+        var complete = this.docRef.where('complete', '==', true);
+        console.log(complete);
+        return complete;
+    }
+}
 
+$(document).ready(function() {
+    var year = new Date().getFullYear();
+    monthli = new Monthli(year);
+    var complete = monthli.getComplete();
+}
+    
