3263e0950501a7fa522b124975d9a0fe47ac3dc3
Avery Herkel <aherkel09@gmail.com>
2019-06-07 16:59:29 -0400

    access firebase auth

--- auth.js
+++ auth.js
@@ -1,1 +1,43 @@
+class Auth {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    createUser() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+            $('#invalid-auth').text(error.message);
+        });
+    }
+    
+    signIn() {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+            $('#invalid-auth').text(error.message);
+        });
+    }
+    
+    signOut() {
+        firebase.auth().signOut().then(function() {
+            $('#content').remove();
+            $('#calendar').remove();
+        }).catch(function(error) {
+            $('#invalid-auth').text(error.message);
+        });
+    }
+}
 
+function submitAuthRequest(requestType) {
+    var auth = new Auth($('#email').val(), $('#password').val());
+    
+    if (requestType == 'signup') {
+        auth.createUser();
+    } else if (requestType == 'login') {
+        auth.signIn();
+    }
+}
+
+$(document).ready(function() {
+    $('#auth').fadeIn('slow');
+}
+    
+    
