5f2471738608ca5c1edba2a1b48723a23a334c1c
Avery Herkel <aherkel09@gmail.com>
2019-06-10 10:29:23 -0400

    create calendar class

--- calendar.js
+++ calendar.js
@@ -1,1 +1,57 @@
+class Calendar {
+    constructor(uid, date, daysComplete) {
+        this.uid = uid;
+        this.date = date;
+        this.daysComplete = daysComplete;
+    }
+    
+    init() {
+        var _this = this;
+        var calendar = jsCalendar.new('#calendar', _this.date);
+        $('.jsCalendar > table').css({
+            'margin': 'auto',
+            'background-color': 'transparent',
+            'color': 'var(--greyscale)',
+            'box-shadow': 'none',
+        });
+        $('.jsCalendar-current')
+            .attr('id', _this.date)
+            .css('color', 'var(--brand)');
+
+        calendar.select(_this.daysComplete);
+        _this.styleSelected();
+
+        calendar.onDateClick(function(event, dateClicked) {
+            $('.jsCalendar-current').removeClass('jsCalendar-current');
+            $(event.target).closest('td').addClass('jsCalendar-current');
+            showDay(dateClicked);
+            _this.styleSelected();
+        });
+
+        calendar.onMonthChange(function(event, dateClicked) {
+            var thisMonth = _this.date.split('-')[1];
+            if (String(dateClicked.getMonth()+1) == thisMonth) {
+                $('#' + today).css('color', 'var(--brand)');
+            } else {
+                $('#' + today).css('color', 'var(--greyscale)');
+            }
+            _this.styleSelected();
+        });
+    }
+
+    showDay(date) {
+        var dateObj = getDate(date); // defined in auth.js
+        var dayli = new Dayli(this.uid, dateObj.year, dateObj.date);
+        dayli.init();
+    }
+
+    styleSelected() {
+        $('td').not('.jsCalendar-current, .jsCalendar-selected')
+            .css('background-color', 'transparent');
+        $('.jsCalendar-current').css('background-color', 'var(--dark)');
+        $('.jsCalendar-selected').css({
+            'background-color': 'var(--accent)',
+            'border': 'none',
+        });
+    }
 
