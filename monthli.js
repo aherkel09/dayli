c9e5384a2260d150aa6ee00afa1ade98d96b93ce
Avery Herkel <aherkel09@gmail.com>
2019-06-10 10:58:59 -0400

    revert

--- monthli.js
+++ monthli.js
@@ -25,8 +25,8 @@ class Monthli {
                 // format completed date to mm-dd-yyyy & push to array
                 complete.push(dateComplete[1] + '-' + dateComplete[0] + '-' + _this.year);
             });
-            dayliCalendar = new Calendar(_this.uid, date, complete); // global Calendar instance
-            dayliCalendar.init();
+            var calendar = new Calendar(_this.uid, date, complete);
+            calendar.init();
         });
     }
 }
